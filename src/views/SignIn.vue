06ef1b3123161fb65ba6be088e911176ac751b56
chinwat kotchum <chinwat@odd.works>
2019-12-23 17:07:59 +0700

    cleanup - init signin

---
+++ src/views/SignIn.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    login
+    <div>
+      <label for="user">Username:</label>
+      <input type="text" id="user" v-model="message" placeholder="" />
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="message" placeholder="" />
+      <button v-on:click="login"> login </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: "signin",
+  data() {
+    return {
+      message: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      console.log('login')
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
