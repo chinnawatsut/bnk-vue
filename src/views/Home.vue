feb71116b39f1a5a99c759c5a6e4f78c9f2aac67
chinwat kotchum <chinwat@odd.works>
2019-12-24 17:01:54 +0700

    show members

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <button v-on:click="logout">Logout</button>
     <router-link to="/profile" >Profile</router-link>
+    <div>
+      <h2>Members</h2>
+      
+      <div v-for="(member,index) in members" :key="index">
+        <h2>{{member.name}}</h2>
+        <h3>IG: {{member.instagramId}}</h3>
+        <img :src="member.imgUrl" :alt="member.name">
+        
+      </div>
+    </div>
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
@@ -16,11 +26,42 @@ export default {
   components: {
     // HelloWorld
   },
+  created() {
+    this.getMember()
+  },
+  data() {
+    return {
+      members: []
+    }
+  },
   methods: {
     logout() {
       localStorage.removeItem("token");
       this.$router.push({ path: "/signin" });
+    },
+    getMember() {
+      let token = localStorage.getItem("token")
+      this.axios
+        .get("http://localhost:3000/bnk/members", {
+          headers: {
+            Authorization: "Bearer " + token
+          }
+        })
+        .then(response => {
+          this.members = response.data
+        })
+        .catch(e => {
+          console.log(e);
+          this.errorMessage = e.response.data;
+        });
     }
   }
 };
 </script>
+
+<style scoped>
+img {
+  width: 100px;
+  height: auto;
+}
+</style>
